c9377a45c3df8a19c01b885998300b85e6783282
Fadi Saif <fadisaif.ba@gmail.com>
2026-02-13 21:21:10 +0300

    Update remote URL and base path for renamed repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/data-science-portfolio/',
+  base: '/Portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
